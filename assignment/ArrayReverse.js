6d6de223272309e877c2c98bbf5a928929414129
popsoft01 <popoolatunde52@gmail.com>
2021-08-03 14:51:17 +0100

    javaScript task

--- assignment/ArrayReverse.js
+++ assignment/ArrayReverse.js
@@ -1,17 +1,28 @@
 function newArray(array){
     return array.reverse()
 }
-function reversal(array ,start, end){
-    for (let i =0; i < array.length; i++){
-        if (start < end){
-            let temp = array[start]
-            array[start] = array[end]
-            array[end] = temp
-            end--
-           return temp;
-        }
+// function reversal(array ,start, end){
+    // for (let i =0; i < array.length; i++){
+    //     if
+    //     while(start < end){
+    //         let temp = array[start]
+    //         array[start] = array[end]
+    //         array[end] = temp
+    //         start++
+    //         end--
+    //        return temp;
+    //     }
+    // let labels = [];
+    // repeat(5, i => {labels.push(`Unit ${i + 1}`);});
+    // console.log(labels);
+function repeat(n, action) {
+    for (let i = 0; i < n; i++) {
+        action(i);
     }
 }
-let a = [1,2,3,4,5]
-console.log(reversal(a,0,array.length-1))
+repeat(3, console.log)
+
+// }
+// let a = [1,2,3,4,5]
+// console.log(reversal(a,0,array.length-1))
 // console.log(b)
